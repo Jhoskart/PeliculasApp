c132e7ab4264c40072a81c433a94268d22b8474c
Jhoskar Toro <jhoskartoro@gmail.com>
2022-11-15 17:32:39 -0300

    se agrega el carousel y flatlist

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import { View, Text, ActivityIndicator } from 'react-native'
+import Carousel from 'react-native-snap-carousel';
+import { View, Text, ActivityIndicator, Dimensions, FlatList } from 'react-native'
 import { useMovies } from '../hooks/useMovies'
+import { MoviePoster } from '../components/MoviePoster'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 const HomeScreen = () => {
 
     const { peliculasEnCine, isLoading } = useMovies();
+    const {top} = useSafeAreaInsets();
+    const { width: windowWidth } = Dimensions.get('window');
 
     if(isLoading) {
         return (
@@ -19,8 +24,44 @@ const HomeScreen = () => {
     }
 
     return (
-        <View>
-            <Text>HomeScreen</Text>
+        <View style={{
+            marginTop: top + 20,
+        }}>
+            {/* <MoviePoster 
+                movie={peliculasEnCine[0]}
+            /> */}
+            <View style={{
+                height: 440,
+            }}>
+            {/* Carousel Principal */}
+            <Carousel
+                data={peliculasEnCine}
+                renderItem={({item}: any) => <MoviePoster movie={item} />}
+                sliderWidth={windowWidth}
+                itemWidth={300}
+            />
+            </View>
+
+            {/* Peliculas Populares */}
+            <View style={{
+                height: 260,
+            }}>
+                <Text style={{
+                    fontSize: 30,
+                    fontWeight: 'bold',
+                    marginLeft: 8,
+                }}>Populares</Text>
+
+                <FlatList 
+                    data={peliculasEnCine}
+                    renderItem={({item}: any) => <MoviePoster movie={item} />}
+                    keyExtractor={(item) => item.id.toString()}
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                />
+            </View>
+
+
         </View>
     )
 }
